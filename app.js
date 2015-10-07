e4cd9c4e9dc4d0a4f91f70b70a66daffaf8afa93
d228m3286w <d228m3286w@gmail.com>
2015-10-07 13:29:54 -0600

    rocks and doughnuts

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var apple;
 var grid;
 
 function displayBanana() {
-	document.write('<em>' + banana  + '</em>');
+	document.write('<em>' + banana.size  + '</em>');
 }; 
 	
 function displayOrange() {
@@ -12,17 +12,49 @@ function displayOrange() {
 };
 	
 function displayApple() {
-	document.write("<em>" + apple + "</em>");
+	document.write("<em>" + apple.yum + "</em>");
 };
 
 function setUp() {
 	banana = new Banana();
-	grid = new Grid(9, 9);
+	// grid = new Grid(9, 9);
 	orange = new Orange();
 	apple = new Apple();
 
 };
 
 
+function name() {
+	var xcoord = prompt("enter how many rows you want");
+	var ycoord = prompt("enter how many columes you want")
+	grid = new Grid(xcoord, ycoord);
+	
+}
+
+// //Adding a function to replace the winning cell with a picture of dead Abe Simpson
+// var killAbe = function () {
+// 	document.getElementById(newAdventure.endY.toString() + newAdventure.endX.toString()).innerHTML = "<img src=\"dead.jpg\"></img>";
+// };
+
+// //Adding a function to place an image at the target cell
+// var target = function(){
+// 	document.getElementById(newAdventure.endY.toString() + newAdventure.endX.toString()).innerHTML = "<img src=\"retire.png\"></img>";
+// };
+    
+// var movAbe = function(){ 
+// 	document.getElementById(newAdventure.yCoord.toString() + newAdventure.xCoord.toString()).innerHTML = "<img src=\"small_abe.png\"></img>";
+// };
+
+// //refactoring hideAbe
+
+// var hideAbe = function (xChange, yChange) {
+// 	document.getElementById((newAdventure.yCoord + yChange).toString() + (newAdventure.xCoord + xChange).toString()).innerHTML = "";
+// }
+
+// var holdon = new Audio('holdon.wav');
+// var coon = new Audio('racoon.wav');
+// var dead = new Audio('dead.wav');
 
 
+// movAbe();
+// target();
--- grid.js
+++ grid.js
@@ -1,32 +1,34 @@
 
 
-function Banana() {
-	this.name = "fanana";
-	this.size = "large";
-	this.toString = function() {
-		return this.name;
-		return this.size;
+// function Banana() {
+// 	this.name = "fanana";
+// 	this.size = "large";
+// 	this.toString = function() {
+// 		return this.name;
+// 		return this.size;
 
-	}
-};
-function Orange() {
-	this.name = "frange";
-	this.size = "ballz";
-	this.toString = function() {
-		return this.name;
-		return this.size;
+// 	}
+// };
+// function Orange() {
+// 	this.name = "frange";
+// 	this.size = "ballz";
+// 	this.toString = function() {
+// 		return this.name;
+// 		return this.size;
 
-	}
-};
-function Apple() {
-	this.name = "fpple";
-	this.size = "large";
-	this.toString = function() {
-		return this.name;
-		return this.size;
+// 	}
+// };
+// function Apple() {
+// 	this.name = "fpple";
+// 	this.size = "large";
+// 	this.yum = "so good!!";
+// 	this.toString = function() {
+// 		return this.name;
+// 		return this.size;
 
-	}
-}; 
+// 	}
+// }; 
+
 
 
 
@@ -35,7 +37,7 @@ function Grid(row,col) {
 	this.col = col;
 	this.makeGrid = function() {
 		for (var rw = 0; rw < this.row; rw++) {
-			document.write('<div class="row">');
+			document.write('<div class="row" >');
 
 
 			for (var cl = 0; cl < this.col; cl++) {
@@ -46,3 +48,85 @@ function Grid(row,col) {
 		}
 	}
 }
+
+// function Adventure(xCoord, yCoord, endX, endY, bound) {
+// 	this.xCoord = xCoord;
+// 	this.yCoord = yCoord;
+// 	this.endX = endX;
+// 	this.endY = endY;
+// 	this.bound = bound;
+
+
+
+
+// };
+// var newAdventure = new Adventure(0, 0, 8, 8, 8);
+
+// newAdventure.mov = function(xDel, yDel, x, y, bound) {
+// 	console.log(newAdventure);
+// 	if(this.xCoord === (this.endX + xDel) && this.yCoord === (this.endY + yDel)) { 
+//         this.xCoord += x;
+//         this.yCoord += y;
+//         hideAbe(xDel, yDel);	
+//         killAbe();
+//         dead.play();
+//         alert("Whuuthaa!!??");
+// 	}
+// 	else if (this.yCoord === bound && yDel != 0) {
+//         holdon.play();
+//         alert("D'oh!!!");
+//     }
+//     else if (this.xCoord === bound && xDel != 0) {
+//         coon.play();
+//         alert("D'oh!!!");
+//     }
+//         else {
+//         this.xCoord += x;
+//         this.yCoord += y;
+// 		hideAbe(xDel, yDel);
+// 		movAbe();
+// 	};
+// }
+
+// newAdventure.movDown = function() {
+// 	this.mov(0, -1, 0, 1, 8);
+// }; 
+// newAdventure.movUp = function() {
+// 	this.mov(0, 1, 0, -1, 0);
+// }; 
+// newAdventure.movLeft = function() {
+// 	this.mov(1, 0, -1, 0, 0);
+// }; 
+// newAdventure.movRight = function() {
+// 	this.mov(-1, 0, 1, 0, 8);
+// };
+
+// newAdventure.reset = function() {
+
+//     hideAbe(0,0);
+//     this.xCoord = 0;
+//     this.yCoord = 0;
+//     movAbe();
+//     target();
+//     console.log('hello');
+// };
+
+// document.onkeydown = function(e) {
+//     switch (e.keyCode) {
+//         case 37:
+//             newAdventure.movLeft();
+//             break;
+//         case 38:
+//             newAdventure.movUp();
+//             break;
+//         case 39:
+//             newAdventure.movRight();
+//             break;
+//         case 40:
+//             newAdventure.movDown();
+// 			break;
+// 		case 32:
+// 			newAdventure.reset();
+
+//     }
+// };
